f910292744963cb484f7ff2b8fd47d166ba19349
saikrishna <saikrishnaveeramachu2003@gmail.com>
2026-04-09 07:18:09 +0530

    first commit

---
+++ Documents/workspace-spring-tools-for-eclipse-4.32.0.RELEASE/zzz/src/main/java/com/example/demo/controller.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class controller {
+	@GetMapping("/welcome")
+	public String getmessage() {
+		return "welcome to docker hub";
+	}
+
+}
